4a8bdd71dcfef2e04633949c494af43da4019f9c
Andrew Hayford <emailandrewhayford@gmail.com>
2018-01-01 14:53:21 -0500

    Basic hashmaps cleanup, separate cargo project to explore hashmap access

--- eight/hashmap/src/main.rs
+++ eight/hashmap/src/main.rs
@@ -10,12 +10,33 @@ fn main() {
     albums.insert(String::from("Easy, Breezy, Beautiful Thugger Girls"), 65);
     albums.insert(String::from("Jeffery"), 100);
 
-    let artists : Vec<String> = Vec::new();
-    Vec.push(String::from("Big K.R.I.T."));
-    Vec.push(String::from("Lil B"));
-    Vec.push(String::from("Young Thug"));
+    let mut artists : Vec<String> = Vec::new();
+    artists.push(String::from("Big K.R.I.T."));
+    artists.push(String::from("Lil B"));
+    artists.push(String::from("Young Thug"));
 
-    
+    let curr_rank : Vec<i32> = vec![3, 5, 4];
 
+    // Get iterator through artists and zip with iterator for rank vector, collect to HashMap
+    let mut artist_ranks : HashMap<_, _> = artists.iter().zip(curr_rank.iter()).collect();
+    println!("{:?}", artist_ranks);
 
+    // References inserted into a HashMap do not have ownership, we can still do below:
+    println!("{:?}", artists);
+
+    // Types that implement copy are copied, ownership is taken for types like Strings
+    let earl : String = String::from("Earl Sweatshirt");
+    let earl_rank : i32 = 2;
+
+    let mut artist_ranks2 : HashMap<String, i32> = HashMap::new();
+    artist_ranks2.insert(earl, earl_rank);
+
+    // Cannot do below as Copy is not implemented, ownership transferred to HashMap
+    //println!("{:?}", earl);
+    println!("{:?}", earl_rank);
+
+    // Loop through each key, value pair
+    for (key, value) in &artist_ranks {
+        println!("{:?} {:?}", key, value);
+    }
 }
